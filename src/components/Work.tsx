539d50a0e0510e836b63f0bb8453ca6db27c0efd
Tushar Wadkar <tusharwadkar@users.noreply.github.com>
2026-04-18 18:36:13 +0530

    mobile responsiive check

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -21,45 +21,48 @@ const projects = [
 
 const Work = () => {
   useGSAP(() => {
-  let translateX: number = 0;
+    let mm = gsap.matchMedia();
 
-  function setTranslateX() {
-    const box = document.getElementsByClassName("work-box");
-    if (box.length === 0) return;
-    const rectLeft = document
-      .querySelector(".work-container")!
-      .getBoundingClientRect().left;
-    const rect = box[0].getBoundingClientRect();
-    const parentWidth = box[0].parentElement!.getBoundingClientRect().width;
-    let padding: number =
-      parseInt(window.getComputedStyle(box[0]).padding) / 2;
-    translateX = rect.width * box.length - (rectLeft + parentWidth) + padding;
-  }
+    mm.add("(min-width: 1025px)", () => {
+      let translateX: number = 0;
 
-  setTranslateX();
+      function setTranslateX() {
+        const box = document.getElementsByClassName("work-box");
+        if (box.length === 0) return;
+        const rectLeft = document.querySelector(".work-container")!
+          .getBoundingClientRect().left;
+        const rect = box[0].getBoundingClientRect();
+        const parentWidth = box[0]
+          .parentElement!.getBoundingClientRect().width;
+        let padding: number =
+          parseInt(window.getComputedStyle(box[0]).padding) / 2;
+        translateX =
+          rect.width * box.length - (rectLeft + parentWidth) + padding;
+      }
 
-  let timeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".work-section",
-      start: "top top",
-      end: `+=${translateX}`,
-      scrub: true,
-      pin: true,
-      id: "work",
-    },
-  });
+      setTranslateX();
 
-  timeline.to(".work-flex", {
-    x: -translateX,
-    ease: "none",
-  });
+      let timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".work-section",
+          start: "top top",
+          end: `+=${translateX}`,
+          scrub: true,
+          pin: true,
+          id: "work",
+        },
+      });
 
-  // Clean up (optional, good practice)
-  return () => {
-    timeline.kill();
-    ScrollTrigger.getById("work")?.kill();
-  };
-}, []);
+      timeline.to(".work-flex", {
+        x: -translateX,
+        ease: "none",
+      });
+    });
+
+    return () => {
+      mm.revert();
+    };
+  }, []);
   return (
     <div className="work-section" id="work">
       <div className="work-container section-container">
